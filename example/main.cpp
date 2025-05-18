8927e38e6529bcacde4bf7ee1bc01f85ec2288ea
Shlomi Nissan <admin@betamark.com>
2025-05-18 13:08:39 -1000

    Add missing 'build and run example' rules

--- example/main.cpp
+++ example/main.cpp
@@ -1,10 +1,10 @@
-#include <print>
+#include <iostream>
 
 #include <cpplib/vector2.hpp>
 
 auto main() -> int {
     const auto v = cpplib::Vector2 {3.0f, 4.0f};
-    std::print("{}\n", v.Length());
+    std::cout << v.Length() << '\n';
 
     return 0;
 }
